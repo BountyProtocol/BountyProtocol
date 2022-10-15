d332311b1b6ecd2885cd4fa303a1ebc5442bf7c9
Roy <toledoroy@gmail.com>
2022-10-15 16:43:15 -0700

    [WIP] Votes support as Game Extension

---
+++ contracts/extensions/VotesExt.sol
@@ -0,0 +1,128 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.4;
+
+// import "hardhat/console.sol";
+
+import "@openzeppelin/contracts-upgradeable/governance/utils/IVotesUpgradeable.sol";
+import "../repositories/interfaces/IVotesRepoTracker.sol";
+import "../abstract/GameExtension.sol";
+// import "../libraries/DataTypes.sol";
+
+/** COPY OF VotesTracker.sol
+ *
+ * @title Game Extension: Votes supports
+ * like Votes / VotesUpgradeable
+ * 
+ * Manged by Reputation
+ * 
+ * Rep[domain] -weight-> Voting power
+ * 
+ */
+contract VotesExt is IVotesUpgradeable, GameExtension {
+
+
+    /// Get the Votes Repo
+    /// @dev Implementing contract must override this!
+    function _votesRepo() internal view virtual returns (IVotesRepoTracker) {
+        revert("Must override _votesRepo()");
+    }
+
+    /* From ERC721Votes
+
+    /**
+     * 
+     * /
+    function _afterTokenTransferTracker(
+        address operator, 
+        uint256 fromSBT, 
+        uint256 toSBT, 
+        uint256[] memory ids,
+        uint256[] memory amounts,
+        bytes memory data
+    ) internal virtual override {
+        // super._afterTokenTransferTracker(
+        //     operator, 
+        //     fromSBT, 
+        //     toSBT, 
+        //     ids,
+        //     amounts,
+        //     data
+        // );
+
+        for (uint256 i = 0; i < ids.length; ++i) {
+            uint256 id = ids[i];
+            if(id == _votingToken){
+                uint256 amount = amounts[i];
+
+                // _transferVotingUnits(from, to, 1);
+
+                //Run this on power changes
+                _votesRepo().transferVotingUnits(fromSBT, toSBT, amount);
+
+            }
+        }
+    }
+
+    /** DOESN'T REALLY NEED THIS...
+     * @dev Returns the balance of `account`.
+     * /
+    function _getVotingUnits(address account) internal view virtual override returns (uint256) {
+        // return balanceOf(account);
+        uint256 sbt = ?
+        _votesRepo().getVotingUnits(sbt);
+    }
+    */
+
+
+
+    //-- IVotes INTERFACE https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.7.3/contracts/governance/utils/IVotes.sol
+    
+    /// Get Current Power
+    /// @dev Returns the current amount of votes that `account` has.
+    function getVotes(address account) external view override returns (uint256) {
+        return _votesRepo().getVotes(account);
+    }
+    
+    /// Get Past Power
+    function getPastVotes(address account, uint256 blockNumber) public view virtual override returns (uint256) {
+        return _votesRepo().getPastVotes(account, blockNumber);
+    }
+
+    /**
+     * @dev Returns the total supply of votes available at the end of a past block (`blockNumber`).
+     *
+     * NOTE: This value is the sum of all available votes, which is not necessarily the sum of all delegated votes.
+     * Votes that have not been delegated are still part of total supply, even though they would not participate in a
+     * vote.
+     */
+    function getPastTotalSupply(uint256 blockNumber) public view virtual override returns (uint256) {
+        return _votesRepo().getPastTotalSupply(blockNumber);
+    }
+
+    /**
+     * @dev Returns the delegate that `account` has chosen.
+     */
+    function delegates(address account) public view virtual override returns (address) {
+        return _votesRepo().delegates(account);
+    }
+    
+    /**
+     * @dev Delegates votes from the sender to `delegatee`.
+     */
+    function delegate(address delegatee) public virtual override {
+        _votesRepo().delegateFrom(msg.sender, delegatee);
+    }
+    // function delegateToToken(uint256 delegateeSBT) public virtual override {
+    //      uint256 fromSBT = getCurrentSBT();
+    //     _votesRepo().delegateFrom(fromSBT, delegateeSBT);
+    // }
+
+    /**
+     * @dev Delegates votes from signer to `delegatee`.
+     */
+    function delegateBySig(address delegatee, uint256 nonce, uint256 expiry, uint8 v, bytes32 r, bytes32 s) public virtual override {
+        _votesRepo().delegateBySig(delegatee, nonce, expiry, v, r, s);
+    }
+
+}
