9d82403134bfb56bd8e2e2df217da902f93c2da5
Roy <toledoroy@gmail.com>
2022-11-15 12:30:21 -0800

    Cleanup

--- scripts/demoData.ts
+++ scripts/demoData.ts
@@ -32,44 +32,30 @@ main().catch((error) => {
 
 const getSoulsData = () => [
     {
-      "id": "1",
-      "type": "",
       "owner": "0x4306d7a79265d2cb85db0c5a55ea5f4f6f73c4b1",
       "uri": "https://ipfs.infura.io/ipfs/QmcQGNWgoba9bQyuB69wPhnxSRg1kunwTUnrNp97NiaLgB"
     },
     {
-      "id": "13",
-      "type": "",
       "owner": "0x70867d94302a4eef645436699607b58122ee9fa9",
       "uri": "https://ipfs.infura.io/ipfs/QmaYLYTWn1DEJMDm5L6P9eybbYARJ8cz3Hcgcq73pqcRsE"
     },
     {
-      "id": "14",
-      "type": "",
       "owner": "0x70d374376c987a3ebcf8168f8ff7f84097664868",
       "uri": "https://ipfs.infura.io/ipfs/QmewTQNaaLTXPNEAX5cZCMYX5mdJMU9r3XWxxmvfTmKjZV"
     },
     {
-      "id": "15",
-      "type": "",
       "owner": "0xe1a71e7ccccc9d06f8bf1cca3f236c0d04da741b",
       "uri": "https://ipfs.infura.io/ipfs/QmNrx3xzYLNFVkmR5z6Hs2F8idLWtjz3mW7zzvcFwvwC99"
     },
     {
-      "id": "4",
-      "type": "",
       "owner": "0x874a6e7f5e9537c4f934fa0d6cea906e24fc287d",
       "uri": "ipfs://Qma8kj5sTEQAgV42hrLqmqj14NvNcaXn2RfgMdzu2M15DJ"
     },
     {
-      "id": "7",
-      "type": "",
       "owner": "0x3f121f9a16bd6c83d325985417ada3fe0f517b7d",
       "uri": "https://ipfs.infura.io/ipfs/QmStWbXsxwDbXVfAKmvg5GYGXGyL1bvVD6xHr6UKEbHCso"
     },
     {
-      "id": "9",
-      "type": "",
       "owner": "0x8b08bda46eb904b18e8385f1423a135167647ca3",
       "uri": "https://ipfs.infura.io/ipfs/QmZXRA5GRS9cgDMMkr5sTdwHr6daruhWmRRgvyM9CD17tX"
     }
