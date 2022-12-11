201b82186045d1c76c6dc11b683e2dd09e718ffa
Roy <toledoroy@gmail.com>
2022-12-11 12:37:02 -0800

    cleanup

--- scripts/_command.ts
+++ scripts/_command.ts
@@ -4,7 +4,7 @@
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
-import { verify, deployContract, deployUUPS, deployGameExt } from "../utils/deployment";
+import { deployContract } from "../utils/deployment";
 const hre = require("hardhat");
 const chain = hre.hardhatArguments.network;
 
