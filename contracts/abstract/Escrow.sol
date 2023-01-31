b3fd02c17371716d8cd25866159ea6d56a2f9d44
Roy <toledoroy@gmail.com>
2023-01-30 17:32:14 -0800

    Cleanup

--- contracts/abstract/Escrow.sol
+++ contracts/abstract/Escrow.sol
@@ -1,12 +1,8 @@
 //SPDX-License-Identifier: MIT
 pragma solidity 0.8.4;
 
-// import "hardhat/console.sol";
-
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
-// import "@openzeppelin/contracts/utils/Context.sol";
-// import "@openzeppelin/contracts-upgradeable/utils/ContextUpgradeable.sol";
 import "../interfaces/IEscrow.sol";
 
 /**
